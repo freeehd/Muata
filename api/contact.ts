c6ba56f0bb7259abd3a471ed7151387e3c393822
agsarim@graduate.utm.my <sarimali@live.com>
2026-04-25 14:10:01 +0500

    muata

--- api/contact.ts
+++ api/contact.ts
@@ -1,6 +1,6 @@
 import type { IncomingMessage, ServerResponse } from 'node:http';
 
-import { parseContactSubmission, sendContactSubmission } from '../lib/contact-mail.ts';
+import { parseContactSubmission, sendContactSubmission } from '../lib/contact-mail.js';
 
 type ApiRequest = IncomingMessage & {
   method?: string;
